6f6f945bf38eff08269dd80433234b0f9684667b
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-11-02 13:19:13 +0900

    Drill11 제출 (2022180041 최재혁)

--- bird.py
+++ bird.py
@@ -11,13 +11,14 @@ class Bird:
         self.bird_width,self.bird_height = 183,168
         self.acc_time = 0.0
         self.bird_frame = 0
-        self.bird_frame_time = 0.2 # 초당 날갯짓 횟수 0.2
+        self.bird_frame_time = 0.2 # 0.2초에 한번 날갯짓 => 초당 5번
         self.x,self.y = 500,500
         # 1픽셀 3센치
         # 1.5m , 1.5m 150cm
         # 50 ,50
         self.bird_width = 50
         self.bird_height = 50
+        self.cur_action_x,self.cur_action_y = 0,0
     def bird_move(self):
         PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
         RUN_SPEED_KMPH = self.bird_kph
@@ -36,9 +37,19 @@ class Bird:
         if self.acc_time >= self.bird_frame_time:
             self.acc_time = 0.0
             self.bird_frame = (self.bird_frame + 1) % 4
+            if self.cur_action_y != 3:
+                temp = (self.cur_action_x + 1)
+                if temp == 5:
+                    self.cur_action_y = (self.cur_action_y + 1) % 3
+                self.cur_action_x = temp % 4
+            else:
+                temp = (self.cur_action_x + 1)
+                if temp == 4:
+                    self.cur_action_y = (self.cur_action_y + 1) % 3
+                self.cur_action_x = temp % 4
     def draw(self):
         dir = 'h' if self.bird_dir != 1 else ''
-        self.bird_img.clip_composite_draw(180 * self.bird_frame,168 * 2,
+        self.bird_img.clip_composite_draw(180 * self.cur_action_x ,168 * (2 - self.cur_action_y),
                                           180,
                                           168,
                                           0,
